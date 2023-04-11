07a99215890abe07e30e4088c51b3c4c31658088
Dave Conrad <eulergroupie@gmail.com>
2023-04-11 08:25:37 -0600

    Create ALongLongTime.js

---
+++ JavaScript/Edabit/Medium/ALongLongTime.js
@@ -0,0 +1,26 @@
+/* A Long Long Time
+Create a function that takes three values:
+
+h hours
+m minutes
+s seconds
+Return the value that's the longest duration.
+
+Examples
+longestTime(1, 59, 3598) ➞ 1
+
+longestTime(2, 300, 15000) ➞ 300
+
+longestTime(15, 955, 59400) ➞ 59400
+Notes
+No two durations will be the same. 
+*/
+
+function longestTime(h, m, c) {
+	const [a, b] = [(h * 3600), (m * 60)];
+    return [h, m, c][(b>=a && b>=c) + 2*(c>=a && c>=b)];
+}
+
+console.log(longestTime(1, 59, 3598), 1)
+console.log(longestTime(2, 300, 15000), 300)
+console.log(longestTime(15, 955, 59400), 59400)
